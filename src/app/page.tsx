b4af141280e16e4992736eb0a01d32ade34b6511
ahmadrasol <ahmad.sam.dev@gmail.com>
2025-05-22 13:51:47 +0800

    hidden upcoming events

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -234,7 +234,7 @@ export default function Home() {
       <AppCarousel></AppCarousel>
       {CreateConsortiumMemberSectionList()}
       {/* {CreateConsortiumMemberSection()} */}
-      {CreateEventList()}
+      {/* {CreateEventList()} */}
       {CreateHighlights()}
       {CreateHeadQuarterSection()}
     </>
